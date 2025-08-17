d1bd99ce4a2634d2f66ef733741a9bf96f46409b
areilly34 <areilly16lax@gmail.com>
2025-08-17 18:26:03 -0400

    Extracted data and created network centrality graph.

--- src/part1_etl.py
+++ src/part1_etl.py
@@ -10,9 +10,24 @@ It is in JSON format, so you'll need to handle accordingly and also figure out w
 import os
 import pandas as pd
 import json
+import requests
 
-# Create '/data' directory if it doesn't exist
-data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
-os.makedirs(data_dir, exist_ok=True)
+def etl():
+    """
+    Extracts json data and saves it to 'data/'
+    Args:
+        none
+    Returns:
+        Saves a json file to 'data/'
+    """
+    
+    # Create '/data' directory if it doesn't exist
+    data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
+    os.makedirs(data_dir, exist_ok=True)
 
-# Load datasets and save to '/data'
+    # Load datasets and save to '/data'
+    response = requests.get(url= 'https://github.com/cbuntain/umd.inst414/blob/main/data/imdb_movies_2000to2022.prolific.json?raw=true')
+
+    # Save data as json file
+    with open('data/data.json', 'w') as file:
+        file.write(response.text)
--- src/part2_network_centrality.py
+++ src/part2_network_centrality.py
@@ -12,45 +12,68 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 import json
+import datetime as dt
 
-# Build the graph
-g = nx.Graph()
+def nc():
+    """
+    Creates a graph that connects actors with eachother based on movie apperances 
+    Args: 
+        none
+    Returns: 
+        Saves a CSV to 'data/'
+    """
+    
+    # Creates graph
+    g = nx.Graph()
+    
+    # Reads in json file
+    with open('data/data.json', 'r') as in_file:
+        for line in in_file:
 
-# Set up your dataframe(s) -> the df that's output to a CSV should include at least the columns 'left_actor_name', '<->', 'right_actor_name'
-
-
-with open() as in_file:
-    # Don't forget to comment your code
-    for line in in_file:
-        # Don't forget to include docstrings for all functions
-
-        # Load the movie from this line
-        this_movie = json.loads(line)
+            # Load the movie from this line
+            this_movie = json.loads(line)
+                
+            # Creates a node for every actor and adds the actor to the graph
+            for actor_id,actor_name in this_movie['actors']:
+                g.add_node(actor_id, name= actor_name)
             
-        # Create a node for every actor
-        for actor_id,actor_name in this_movie['actors']:
-        # add the actor to the graph    
-        # Iterate through the list of actors, generating all pairs
-        ## Starting with the first actor in the list, generate pairs with all subsequent actors
-        ## then continue to second actor in the list and repeat
-        
-        i = 0 #counter
-        for left_actor_id,left_actor_name in this_movie['actors']:
-            for right_actor_id,right_actor_name in this_movie['actors'][i+1:]:
+            i = 0 #counter
+            for left_actor_id,left_actor_name in this_movie['actors']:
+                for right_actor_id,right_actor_name in this_movie['actors'][i+1:]:
+                    
+                     # Get the current weight, if it exists
+                    if g.has_edge(left_actor_id, right_actor_id):
+                        current_edge = g[left_actor_id][right_actor_id]
+                        current_weight = current_edge['weight']
+                    else:
+                        current_weight = 0
+                    
+                     # Add an edge between left and right actor
+                    g.add_edge(left_actor_id, right_actor_id, weight= current_weight + 1)
+                    i += 1
 
-                # Get the current weight, if it exists
-                
-                
-                # Add an edge for these actors
-                
-                
+    print("Nodes:", len(g.nodes))
 
+    # Gets degree of centrality 
+    deg_cent = nx.degree_centrality(g)
 
-# Print the info below
-print("Nodes:", len(g.nodes))
+    # Creates a df for actor centrality
+    centrality_data = []
+    for node_id in g.nodes():
+        node_name = g.nodes[node_id]['name']
+        centrality_data.append({
+            'actor_id': node_id,
+            'actor_name': node_name,
+            'degree_centrality': deg_cent[node_id]
+        })
 
-#Print the 10 the most central nodes
+    centrality_df = pd.DataFrame(centrality_data)
+    centrality_df = centrality_df.sort_values('degree_centrality', ascending=False)
 
+    print('10 most central nodes:')
+    print(centrality_df[['actor_name', 'degree_centrality']].head(10))
 
-# Output the final dataframe to a CSV named 'network_centrality_{current_datetime}.csv' to `/data`
+    # Saves centrality_df to 'data/'
+    current_dt = dt.datetime.now().strftime("%Y%m%d%H%M%S")
+    centrality_df.to_csv(f'data/network_centrality_{current_dt}.csv')
 
